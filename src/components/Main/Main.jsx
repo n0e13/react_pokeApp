f21f09e996bf7338c9fbed34aeba34b080d5c74d
n0e <noemy.garcia@gmail.com>
2022-05-02 08:25:49 +0200

    [ADD] Files

---
+++ src/components/Main/Main.jsx
@@ -0,0 +1,16 @@
+import React, { useState } from "react";
+import Card from './Card';
+
+const Main = () => {
+
+
+  return (
+    <div>
+      <p>Busca tu Pokemon</p>
+      <Card />
+    </div>
+  );
+
+}
+
+export default Main;
